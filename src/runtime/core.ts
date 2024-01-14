7376d69fb076310366a11284dd70c47c4097d89a
fcapolini <fabrizio.capolini@me.com>
2024-01-14 18:26:20 +0100

    runtime/core

--- src/runtime/core.ts
+++ src/runtime/core.ts
@@ -1,6 +1,7 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/with#creating_dynamic_namespaces_using_the_with_statement_and_a_proxy
 
 const SCOPE_KEY = '$scope';
+const VALUE_KEY = '$value';
 const UNINITED = Symbol('uninited');
 
 /**
@@ -17,12 +18,24 @@ export class Context {
   constructor() {
     this.global = new Scope(this, globalThis);
     this.cycle = 0;
-    this.refreshLevel = 0;
-    this.pushLevel = 0;
+    this.refreshLevel = this.pushLevel = 0;
   }
 
   refresh(scope?: Scope) {
-
+    if (!scope && !(scope = this.root)) {
+      return this;
+    }
+    this.refreshLevel++;
+    try {
+      this.cycle++;
+      scope.unlinkValues();
+      scope.linkValues();
+      scope.updateValues();
+    } catch (err: any) {
+      console.error('Context.refresh()', err);
+    }
+    this.refreshLevel--;
+    return this;
   }
 }
 
@@ -33,15 +46,18 @@ export class Scope {
   context: Context;
   parent?: Scope;
   children: Scope[];
+  values: Map<string, Value>;
   object: any;
   proxy: any;
 
   constructor(context: Context, proto?: any) {
     this.context = context;
     this.children = [];
+    this.values = new Map();
     this.object = Object.create(proto || null);
     this.proxy = Scope.initProxy(this.object);
     this.object[SCOPE_KEY] = this;
+    this.object[VALUE_KEY] = (key: string) => this.lookupValue(key);
   }
 
   link(parent: Scope, name?: string): this {
@@ -54,27 +70,62 @@ export class Scope {
     return this;
   }
 
-  unlink() {
+  unlink(recursively = false) {
     if (!this.parent) {
       return;
     }
+    this.unlinkValues();
+    const i = this.parent.children.indexOf(this);
+    i >= 0 && this.parent.children.splice(i, 1);
+    this.parent = undefined;
+    if (recursively) {
+      while (this.children.length > 0) {
+        this.children[this.children.length - 1].unlink();
+      }
+    }
+  }
+
+  unlinkValues() {
+    this.values.forEach(v => v.unlink());
+    this.children.forEach(s => s.unlinkValues());
+  }
+
+  linkValues() {
+    this.values.forEach(v => v.link());
+    this.children.forEach(s => s.linkValues());
+  }
+
+  updateValues() {
+    this.values.forEach((_, k) => this.proxy[k]);
+    this.children.forEach(s => s.updateValues());
+  }
+
+  lookupValue(key: string): Value | undefined {
+    let scope: Scope | undefined = this;
+    let value = undefined;
+    while (scope && !value) {
+      value = scope.values.get(key);
+      scope = scope.parent;
+    }
+    return value instanceof Value ? value : undefined;
+  }
+
+  static lookupProperty(target: any, prop: string) {
+    let ret = target[prop];
+    if (ret === undefined) {
+      for (let scope = target[SCOPE_KEY]; scope; scope = scope.parent) {
+        if ((ret = scope.object[prop]) !== undefined) {
+          break;
+        }
+      }
+    }
+    return ret;
   }
 
   static initProxy(object: any): any {
-    function lookup(target: any, prop: string) {
-      let ret = target[prop];
-      if (ret === undefined) {
-        for (let scope = target[SCOPE_KEY]; scope; scope = scope.parent) {
-          if ((ret = scope.object[prop]) !== undefined) {
-            break;
-          }
-        }
-      }
-      return ret;
-    }
     return new Proxy(object, {
       get: (target: any, prop: string, receiver?: any) => {
-        let ret = lookup(target, prop);
+        let ret = Scope.lookupProperty(target, prop);
         if (ret instanceof Value) {
           ret = ret.get();
         }
@@ -84,6 +135,9 @@ export class Scope {
   }
 }
 
+type ValueExp = () => any;
+type ValueRef = () => Value | undefined;
+
 /**
  * Value in a scope in a reactive context.
  */
@@ -91,17 +145,18 @@ export class Value {
   context: Context;
   scope: Scope;
 
-  exp: () => any;
-  ref?: string[];
+  exp: ValueExp;
+  ref?: ValueRef[];
+  cb?: (v: any) => any;
+
   src?: Set<Value>;
   dst?: Set<Value>;
-  cb?: (v: any) => any;
 
   cycle: number;
   v1: any;
   v2: any;
 
-  constructor(context: Context, scope: Scope, exp: () => any, ref?: string[]) {
+  constructor(context: Context, scope: Scope, exp: ValueExp, ref?: ValueRef[]) {
     this.context = context;
     this.scope = scope;
     this.exp = exp;
@@ -110,10 +165,29 @@ export class Value {
     this.v1 = UNINITED;
   }
 
+  link() {
+    this.ref?.forEach(ref => {
+      let that = undefined;
+      try {
+        that = runExp(this.scope.proxy, ref);
+        if (that === this) {
+          that = this.scope.parent ? runExp(this.scope.parent.proxy, ref) : undefined;
+        }
+      } catch (ignored: any) {}
+      if (that) {
+        (this.src ?? (this.src = new Set())).add(that);
+        (that.dst ?? (that.dst = new Set())).add(this);
+      }
+    });
+  }
+
+  unlink() {
+    this.src?.forEach(o => o?.dst?.delete(this));
+    delete this.src;
+  }
+
   get(): any {
-    if (this.cycle !== this.context.cycle) {
-      this.refresh();
-    }
+    this.cycle === this.context.cycle || this.update();
     return this.v2;
   }
 
@@ -121,7 +195,7 @@ export class Value {
 
   }
 
-  refresh() {
+  private update() {
     this.cycle = this.context.cycle;
     const old = this.v1;
     try {
@@ -140,7 +214,7 @@ export class Value {
     }
     context.pushLevel++;
     try {
-      this.dst?.forEach(v => v.refresh());
+      this.dst?.forEach(v => v.update());
     } catch (ignored: any) {}
     context.pushLevel--;
   }
--- test/runtime/core.test.ts
+++ test/runtime/core.test.ts
@@ -5,12 +5,12 @@ describe('runtime: core', function () {
 
   describe('global scope', () => {
 
-    it("1", () => {
+    it("can see globals", () => {
       const ctx = new Context();
       assert.equal(ctx.global.proxy['console'], console);
     });
 
-    it("2", () => {
+    it("can see added global", () => {
       const ctx = new Context();
       globalThis['pageLogicTest'] = 1;
       assert.equal(ctx.global.proxy['pageLogicTest'], 1);
@@ -18,7 +18,7 @@ describe('runtime: core', function () {
       assert.notExists(ctx.global.proxy['pageLogicTest']);
     });
 
-    it("3", () => {
+    it("can add app globals without polluting", () => {
       const ctx = new Context();
       ctx.global.proxy['pageLogicTest'] = 1;
       assert.notExists(globalThis['pageLogicTest']);
@@ -31,14 +31,55 @@ describe('runtime: core', function () {
 
   describe('root scope', () => {
 
-    it("1", () => {
-      const ctx = new Context();
-      const root = new Scope(ctx);
-      root.link(ctx.global, 'page');
-      assert.equal(ctx.global.proxy['page'], root.proxy);
-      assert.equal(root.proxy['page'], root.proxy);
+    it("is visible from global by name", () => {
+      const { context, root } = contextWithRoot();
+      assert.equal(context.global.proxy['root'], root.proxy);
+      assert.equal(root.proxy['root'], root.proxy);
+    });
+
+    it("can see app globals", () => {
+      const { context, root } = contextWithRoot();
+      context.global.proxy['pageLogicTest'] = 1;
+      assert.equal(root.proxy['pageLogicTest'], 1);
+    });
+
+  });
+
+  describe('child scope', () => {
+
+    it("is visible from global by name", () => {
+      const { context, root } = contextWithRoot();
+      const child = new Scope(context);
+      child.link(root, 'child');
+      assert.equal(context.global.proxy['root']['child'], child.proxy);
+      assert.equal(root.proxy['child'], child.proxy);
+      assert.equal(child.proxy['child'], child.proxy);
+    });
+
+    it("can see app globals", () => {
+      const { context, root } = contextWithRoot();
+      const child = new Scope(context);
+      child.link(root, 'child');
+      context.global.proxy['pageLogicTest'] = 1;
+      assert.equal(child.proxy['pageLogicTest'], 1);
+    });
+
+  });
+
+  describe('scope values', () => {
+
+    it("can add value", () => {
+      const { context, root } = contextWithRoot();
+
     });
 
   });
 
 });
+
+function contextWithRoot(): { root: Scope, context: Context } {
+  const context = new Context();
+  const root = new Scope(context);
+  root.link(context.global, 'root');
+  return { root, context };
+}
