bf001ab479ca4c5c83bd9d8839cfae703f67ab84
fcapolini <fabrizio.capolini@me.com>
2024-03-19 00:58:31 +0100

    compiler

--- packages/pagelogic/src/compiler/utils.ts
+++ packages/pagelogic/src/compiler/utils.ts
@@ -24,15 +24,81 @@ export class Stack<T> extends Array<T> {
   }
 }
 
-export function object(ref: html.Node): acorn.ObjectExpression {
+function loc(ref: html.Node) {
   return {
-    type: 'ObjectExpression',
-    properties: [],
-    start: ref.loc.i1, end: ref.loc.i2,
+    start: ref.loc.i1,
+    end: ref.loc.i2,
     loc: {
       source: ref.loc.source,
       start: ref.loc.start,
       end: ref.loc.end
     }
+  };
+}
+
+export function object(ref: html.Node): acorn.ObjectExpression {
+  return {
+    type: 'ObjectExpression',
+    properties: [],
+    ...loc(ref),
+  };
+}
+
+export function property(key: string, val: acorn.Expression, ref: html.Node): acorn.Property {
+  return {
+    type: 'Property',
+    method: false,
+    shorthand: false,
+    computed: false,
+    key: identifier(key, ref),
+    value: val,
+    kind: 'init',
+    ...loc(ref),
+  };
+}
+
+export function identifier(key: string, ref: html.Node): acorn.Identifier {
+  return {
+    type: 'Identifier',
+    name: key,
+    ...loc(ref),
+  };
+}
+
+export function array(ref: html.Node): acorn.ArrayExpression {
+  return {
+    type: 'ArrayExpression',
+    elements: [],
+    ...loc(ref),
+  };
+}
+
+export function literal(value: string | number, ref: html.Node): acorn.Literal {
+  return {
+    type: 'Literal',
+    value,
+    ...loc(ref),
+  };
+}
+
+export function fnExpression(exp: acorn.Literal | acorn.Expression, ref: html.Node): acorn.FunctionExpression {
+  return {
+    type: 'FunctionExpression',
+    expression: false,
+    generator: false,
+    async: false,
+    params: [],
+    body: {
+      type: 'BlockStatement',
+      body: [
+        {
+          type: 'ReturnStatement',
+          argument: exp,
+          ...loc(ref),
+        }
+      ],
+      ...loc(ref),
+    },
+    ...loc(ref),
   };
 }
