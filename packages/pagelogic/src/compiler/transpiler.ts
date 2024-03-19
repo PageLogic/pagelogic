5fd53c7559c77b44f6d7f0d7bb650f44fe595eb7
fcapolini <fabrizio.capolini@me.com>
2024-03-19 16:13:57 +0100

    compiler: transpiler

--- packages/pagelogic/src/compiler/transpiler.ts
+++ packages/pagelogic/src/compiler/transpiler.ts
@@ -5,48 +5,59 @@ import { Source } from './types';
 import { Stack } from './utils';
 
 export function transpile(source: Source) {
-
-  function genValue(key: string, val: string | html.Attribute, scope: Logic, stack: Stack<Logic>) {
-    const ref = typeof val === 'string' ? scope.ref : (val as html.Attribute);
-    const ret = utils.object(ref);
-    // ret.properties.push(utils.property('fn', utils.fnExpression(
-
-    // ), ref));
-    return ret;
-  }
-
-  function genValues(scope: Logic, stack: Stack<Logic>) {
-    const ref = scope.ref;
-    const ret = utils.object(ref);
-    for (const key of Reflect.ownKeys(scope.vv) as string[]) {
-      ret.properties.push(utils.property(
-        key,
-        genValue(key, scope.vv[key], scope, stack), ref)
-      );
-    }
-    return ret;
-  }
-
-  function genScope(scope: Logic, stack: Stack<Logic>) {
-    const ref = scope.ref;
-    const ret = utils.object(ref);
-    // ret.properties.push(utils.property('id', utils.literal(scope.id, ref), ref));
-    // ret.properties.push(utils.property('values', genValues(scope, stack), ref));
-    return ret;
-  }
-
-  function genScopes(scope: Logic, stack: Stack<Logic>) {
-    const ret = genScope(scope, stack);
-    stack.push(scope);
-    try {
-      scope.cc.forEach(child => genScopes(child, stack));
-    } catch (ignored) { /* nop */ }
-    stack.pop();
-    return ret;
-  }
-
   if (source.logic && source.errors.length < 1) {
-    const ast = genScopes(source.logic, new Stack<Logic>());
+    const ast = genScopes(source, source.logic, new Stack<Logic>());
     source.ast = ast;
   }
+}
+
+function genScopes(source: Source, scope: Logic, stack: Stack<Logic>) {
+  const ret = genScope(source, scope, stack);
+  stack.push(scope);
+  try {
+    scope.cc.forEach(child => genScopes(source, child, stack));
+  } catch (ignored) { /* nop */ }
+  stack.pop();
+  return ret;
+}
+
+function genScope(source: Source, scope: Logic, stack: Stack<Logic>) {
+  const ref = scope.ref;
+  const ret = utils.object(ref);
+  // ret.properties.push(utils.property(
+  //   'id',
+  //   utils.literal(scope.id, ref), ref)
+  // );
+  // ret.properties.push(utils.property(
+  //   'values',
+  //   genValues(source, scope, stack), ref)
+  // );
+  return ret;
+}
+
+function genValues(source: Source, scope: Logic, stack: Stack<Logic>) {
+  const ref = scope.ref;
+  const ret = utils.object(ref);
+  for (const key of Reflect.ownKeys(scope.vv) as string[]) {
+    ret.properties.push(utils.property(
+      key,
+      genValue(source, scope, stack, key, scope.vv[key]), ref)
+    );
+  }
+  return ret;
+}
+
+function genValue(
+  source: Source, scope: Logic, stack: Stack<Logic>,
+  key: string, val: string | html.Attribute
+) {
+  const ref = typeof val === 'string' ? scope.ref : (val as html.Attribute);
+  const ret = utils.object(ref);
+  // ret.properties.push(utils.property(
+  //   'fn',
+  //   utils.fnExpression(
+
+  //   ), ref)
+  // );
+  return ret;
 }
