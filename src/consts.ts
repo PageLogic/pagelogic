8d5d0fff7762278d844af4374a3eb28c7f20c834
fcapolini <fabrizio.capolini@me.com>
2024-08-28 21:19:33 +0200

    cleanup

--- src/consts.ts
+++
@@ -1,3 +0,0 @@
-// export const SRC_CLIENT_CODE = 'runtime.js';
-export const SRC_CLIENT_CODE = 'runtime.min.js';
-export const DST_CLIENT_CODE = '/pagelogic-rt.js';
--- test/code/loader/directives/testTextualInclude/include.js
+++
@@ -1,1 +0,0 @@
-console.log('hi'.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/"/g,"&quot;").replace(/'/g,"&#x27;"))
