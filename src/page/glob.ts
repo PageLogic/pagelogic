65057a60e2cff63fe4217c4318e13789fb9bfd3e
fcapolini <fabrizio.capolini@me.com>
2024-09-19 11:22:56 +0200

    runtime

--- src/page/glob.ts
+++ src/page/glob.ts
@@ -1,10 +1,13 @@
 import { Document } from '../html/dom';
+import { PageProps } from './props';
 import { Scope } from './scope';
 
 export abstract class Glob extends Scope {
+  props?: PageProps;
 
-  constructor(doc: Document) {
+  constructor(doc: Document, props?: PageProps) {
     super(-1, doc);
+    this.props = props;
     this.init();
   }
 
---
+++ test/runtime/base.test.ts
@@ -0,0 +1,32 @@
+import { assert } from 'chai';
+import { generate } from 'escodegen';
+import { CompilerGlob } from '../../src/compiler/compiler-glob';
+import { CompilerPage } from '../../src/compiler/compiler-page';
+import { parse } from '../../src/html/parser';
+import { Page } from '../../src/page/page';
+import { RuntimePage } from '../../src/runtime/runtime-page';
+import { ServerGlob } from '../../src/server/server-glob';
+
+describe('runtime/base', () => {
+
+  it('001', () => {
+    const page = load('<html></html>');
+    console.log(page.glob.doc.toString());//tempdebug
+    console.log(page.glob.props);//tempdebug
+    //TODO
+  });
+
+});
+
+function load(html: string): Page {
+  const src = parse(html, 'test');
+  assert.equal(src.errors.length, 0);
+  const compilerGlob = new CompilerGlob(src.doc);
+  const compilerPage = new CompilerPage(compilerGlob);
+  assert.equal(compilerPage.errors.length, 0);
+  const js = generate(compilerPage.ast);
+  const props = eval(`(${js})`);
+  const serverGlob = new ServerGlob(src.doc, props);
+  const runtimePage = new RuntimePage(serverGlob);
+  return runtimePage;
+}
