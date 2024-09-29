fbb9f45e66159ba3564b34b862cd42416fb4de5a
fcapolini <fabrizio.capolini@me.com>
2024-09-29 18:02:27 +0200

    server

--- test/server/server.test.ts
+++ test/server/server.test.ts
@@ -21,12 +21,14 @@ describe('server', () => {
   describe('server/ssr+!csr', () => {
     let server: Server;
     let browser: Browser;
+
     before(async () => {
       server = await new Server({
         docroot, mute: true, ssr: true, csr: false
       }).start();
       browser = new Browser();
     });
+
     after(async () => {
       await browser.close();
       await server.stop();
@@ -53,6 +55,96 @@ describe('server', () => {
       assert.equal(page.mainFrame.document.title, 'Page Error');
       page = await load(server.port!, '/fragment.htm');
       assert.equal(page.content.replace(/<.*?>/g, ''), 'Not Found');
+    });
+
+    it('/text.txt', async () => {
+      const page = await load(server.port!, '/');
+      const res = await page.mainFrame.window.fetch(
+        `http://127.0.0.1:${server.port}/text.txt`
+      );
+      assert.equal(res.status, 200);
+      const txt = await res.body?.getReader().read();
+      assert.equal(txt?.value.toString(), 'some text in /');
+    });
+
+    it('/visible/index', async () => {
+      let page = await load(server.port!, '/visible');
+      assert.equal(page.mainFrame.document.title, '/visible/index');
+      page = await load(server.port!, '/visible/');
+      assert.equal(page.mainFrame.document.title, '/visible/index');
+      page = await load(server.port!, '/visible/index');
+      assert.equal(page.mainFrame.document.title, '/visible/index');
+      page = await load(server.port!, '/visible/index.html');
+      assert.equal(page.mainFrame.document.title, '/visible/index');
+    });
+
+    it('/visible/other', async () => {
+      let page = await load(server.port!, '/visible/other');
+      assert.equal(page.mainFrame.document.title, '/visible/other');
+      page = await load(server.port!, '/visible/other.html');
+      assert.equal(page.mainFrame.document.title, '/visible/other');
+    });
+
+    it('/visible/fragment', async () => {
+      let page = await load(server.port!, '/visible/fragment');
+      assert.equal(page.mainFrame.document.title, 'Page Error');
+      page = await load(server.port!, '/visible/fragment.htm');
+      assert.equal(page.content.replace(/<.*?>/g, ''), 'Not Found');
+    });
+
+    it('/visible/text.txt', async () => {
+      const page = await load(server.port!, '/');
+      const res = await page.mainFrame.window.fetch(
+        `http://127.0.0.1:${server.port}/visible/text.txt`
+      );
+      assert.equal(res.status, 200);
+      const txt = await res.body?.getReader().read();
+      assert.equal(txt?.value.toString(), 'some text in /visible');
+    });
+
+    it('/.hidden/index', async () => {
+      let page = await load(server.port!, '/.hidden');
+      assert.equal(page.content.replace(/<.*?>/g, ''), 'Not Found');
+      page = await load(server.port!, '/.hidden/');
+      assert.equal(page.content.replace(/<.*?>/g, ''), 'Not Found');
+      page = await load(server.port!, '/.hidden/index');
+      assert.equal(page.content.replace(/<.*?>/g, ''), 'Not Found');
+      page = await load(server.port!, '/.hidden/index.html');
+      assert.equal(page.content.replace(/<.*?>/g, ''), 'Not Found');
+    });
+
+    it('/.hidden/other', async () => {
+      let page = await load(server.port!, '/.hidden/other');
+      assert.equal(page.content.replace(/<.*?>/g, ''), 'Not Found');
+      page = await load(server.port!, '/.hidden/other.html');
+      assert.equal(page.content.replace(/<.*?>/g, ''), 'Not Found');
+    });
+
+    it('/.hidden/fragment', async () => {
+      let page = await load(server.port!, '/.hidden/fragment');
+      assert.equal(page.content.replace(/<.*?>/g, ''), 'Not Found');
+      page = await load(server.port!, '/.hidden/fragment.htm');
+      assert.equal(page.content.replace(/<.*?>/g, ''), 'Not Found');
+    });
+
+    it('/.hidden/text.txt', async () => {
+      const page = await load(server.port!, '/');
+      const res = await page.mainFrame.window.fetch(
+        `http://127.0.0.1:${server.port}/.hidden/text.txt`
+      );
+      assert.equal(res.status, 404);
+    });
+
+    it('/folder/index', async () => {
+      // /folder/ directory has priority over /folder.html page
+      let page = await load(server.port!, '/folder');
+      assert.equal(page.mainFrame.document.title, '/folder/index');
+      page = await load(server.port!, '/folder/');
+      assert.equal(page.mainFrame.document.title, '/folder/index');
+      page = await load(server.port!, '/folder/index');
+      assert.equal(page.mainFrame.document.title, '/folder/index');
+      page = await load(server.port!, '/folder/index.html');
+      assert.equal(page.mainFrame.document.title, '/folder/index');
     });
 
     it(CLIENT_CODE_REQ, async () => {
